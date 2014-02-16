39b7405f1edb16c0f66abb5268626c07e130cef6
Steven Normore <snormore@gmail.com>
2014-02-16 16:04:53 +0000

    Adds config initialization.

--- config.go
+++ config.go
@@ -1,15 +1,37 @@
 package wire
 
 type Config struct {
-	NumberOfInputters  int         `json:"number_of_inputters"`
-	NumberOfOutputters int         `json:"number_of_outputters"`
-	BufferSize         int         `json:"buffer_size"`
-	In                 interface{} `json:"in"`
-	Out                interface{} `json:"out"`
+	NumberOfInputters  int `json:"number_of_inputters"`
+	NumberOfOutputters int `json:"number_of_outputters"`
+	BufferSize         int `json:"buffer_size"`
 }
 
 var DefaultConfig = Config{
 	NumberOfInputters:  10,
 	NumberOfOutputters: 10,
 	BufferSize:         1024,
+}
+
+func NewConfig(rawConfig map[string]interface{}) (*Config, error) {
+	config := new(Config)
+
+	if _, ok := rawConfig["number_of_inputters"]; !ok {
+		config.NumberOfInputters = DefaultConfig.NumberOfInputters
+	} else {
+		config.NumberOfInputters = rawConfig["number_of_inputters"].(int)
+	}
+
+	if _, ok := rawConfig["number_of_outputters"]; !ok {
+		config.NumberOfOutputters = DefaultConfig.NumberOfOutputters
+	} else {
+		config.NumberOfOutputters = rawConfig["number_of_outputters"].(int)
+	}
+
+	if _, ok := rawConfig["buffer_size"]; !ok {
+		config.BufferSize = DefaultConfig.BufferSize
+	} else {
+		config.BufferSize = rawConfig["buffer_size"].(int)
+	}
+
+	return config, nil
 }
